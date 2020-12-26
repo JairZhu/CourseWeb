19f2381ec3550c5044aad9b3777ebee221052a8e
JairZhu <739741104@qq.com>
2020-12-26 16:57:58 +0800

    对框架进行微调

--- CourseWeb/src/App.vue
+++ CourseWeb/src/App.vue
@@ -10,24 +10,16 @@
       </el-col>
     </el-row>
     <el-row type="flex" justify="center">
-      <el-col :span="11">
-        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-          <el-menu-item index="mainPage">首页</el-menu-item>
-          <el-menu-item index="newsCenter">新闻中心</el-menu-item>
-          <el-menu-item index="notifications">通知公告</el-menu-item>
-          <el-menu-item index="syllabus">教学大纲</el-menu-item>
-          <el-menu-item index="courseware">课件下载</el-menu-item>
-          <el-menu-item index="homeworkDownload">作业下载</el-menu-item>
-          <el-menu-item index="discuss">在线答疑</el-menu-item>
-          <el-menu-item index="homeworkSubmit">作业提交</el-menu-item>
-        </el-menu>
-      </el-col>
-      <el-col :span="2">
-        <div style="margin-top: 10px">
-          <el-button type="text">登录</el-button>
-          <el-button type="text">注册</el-button>
-        </div>
-      </el-col>
+      <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+        <el-menu-item index="mainPage">首页</el-menu-item>
+        <el-menu-item index="newsCenter">新闻中心</el-menu-item>
+        <el-menu-item index="notifications">通知公告</el-menu-item>
+        <el-menu-item index="syllabus">教学大纲</el-menu-item>
+        <el-menu-item index="courseware">课件下载</el-menu-item>
+        <el-menu-item index="homeworkDownload">作业下载</el-menu-item>
+        <el-menu-item index="discuss">在线答疑</el-menu-item>
+        <el-menu-item index="homeworkSubmit">作业提交</el-menu-item>
+      </el-menu>
     </el-row>
     <el-row>
       <router-view/>
