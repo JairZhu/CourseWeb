741e44787b218b0c3cb0219ee80233bed792f45b
JairZhu <739741104@qq.com>
2020-12-26 20:57:15 +0800

    fix 起始页重定向问题

--- CourseWeb/src/router/index.js
+++ CourseWeb/src/router/index.js
@@ -9,6 +9,10 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path:'/',
+      redirect: '/mainPage'
+    },
+    {
       path:'/mainPage',
       name:'mainPage',
       component: MainPage
