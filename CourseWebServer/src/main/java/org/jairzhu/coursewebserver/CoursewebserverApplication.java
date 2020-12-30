492e40aa06d703ccd683f324ea859ea080b3bde1
JairZhu <739741104@qq.com>
2020-12-30 17:11:29 +0800

    搭建简单的后端

---
+++ CourseWebServer/src/main/java/org/jairzhu/coursewebserver/CoursewebserverApplication.java
@@ -0,0 +1,13 @@
+package org.jairzhu.coursewebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursewebserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoursewebserverApplication.class, args);
+    }
+
+}
---
+++ CourseWebServer/src/test/java/org/jairzhu/coursewebserver/CoursewebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jairzhu.coursewebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursewebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
