dd2f3f75e6cddd06df5944903cd89d2e66b8c738
shaoqiangzou <1006243223@qq.com>
2021-06-08 18:04:30 +0800

    更改头像显示，修改逻辑

--- CourseWebServer/src/main/java/org/jairzhu/coursewebserver/controller/UserInfoController.java
+++ CourseWebServer/src/main/java/org/jairzhu/coursewebserver/controller/UserInfoController.java
@@ -1,33 +1,38 @@
 package org.jairzhu.coursewebserver.controller;
 
 
-import org.jairzhu.coursewebserver.domain.Comment;
+import org.jairzhu.coursewebserver.domain.NonStaticResourceHttpRequestHandler;
 import org.jairzhu.coursewebserver.domain.User;
 import org.jairzhu.coursewebserver.mapper.CourseWebMapper;
-import org.jairzhu.coursewebserver.redisUtils.RedisHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.util.ResourceUtils;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
-
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @EnableAutoConfiguration
 public class UserInfoController {
     private final Logger logger = LoggerFactory.getLogger(CourseWebController.class);
+    private final NonStaticResourceHttpRequestHandler nonStaticResourceHttpRequestHandler;
+
+    public UserInfoController(NonStaticResourceHttpRequestHandler nonStaticResourceHttpRequestHandler, CourseWebMapper courseWebMapper) {
+        this.nonStaticResourceHttpRequestHandler = nonStaticResourceHttpRequestHandler;
+        this.courseWebMapper = courseWebMapper;
+    }
 
     @Autowired
     private CourseWebMapper courseWebMapper;
@@ -55,28 +60,20 @@ public class UserInfoController {
     public boolean uploadHeadPhoto(@PathVariable(name = "userName") String userName,@RequestParam(value = "file") MultipartFile file,HttpServletRequest req) {
 
         if (!file.isEmpty()) {
-            String fileName = userName+".jpg";
-            String filePath = System.getProperty("user.dir") + "/src/main/resources/static/headPhoto";
-            System.out.println(filePath);
+            String fileName = userName;
+
+
+            String staticPath = System.getProperty("user.dir") + "/src/main/resources/static/headPhoto";
+            System.out.println(staticPath);
+
+            File dir =new File(staticPath);
+            if  (!dir.exists()  && !dir.isDirectory())
+            {
+                dir.mkdirs();
+            }
+
             try {
-                String staticPath=filePath+"/"+userName+".jpg";
-                filePath=ResourceUtils.getURL("classpath:").getPath()+"static/headPhoto";
-                file.transferTo(new File(filePath, fileName));
-                String servePath=filePath+"/"+userName+".jpg";
-                InputStream input = null;
-                OutputStream output = null;
-                try {
-                    input = new FileInputStream(servePath);
-                    output = new FileOutputStream(staticPath);
-                    byte[] buf = new byte[1024];
-                    int bytesRead;
-                    while ((bytesRead = input.read(buf)) > 0) {
-                        output.write(buf, 0, bytesRead);
-                    }
-                } finally {
-                    input.close();
-                    output.close();
-                }
+                file.transferTo(new File(staticPath, fileName));
             } catch (IOException e) {
                 e.printStackTrace();
             }
@@ -88,13 +85,28 @@ public class UserInfoController {
     }
 
 
-    @RequestMapping("/getHeadPhoto")
-    @ResponseBody
-    public void getHeadPhoto() throws IOException {
 
+    @RequestMapping(value = "photoPlay")
+    @ResponseBody
+    public void videoPreview(@RequestParam(name = "userName") String userName, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        String path = ResourceUtils.getURL("src/main/resources/static/headPhoto").getPath().substring(1);
+        String realPath = path + userName;
+
+        Path filePath = Paths.get(realPath);
+
+        if (Files.exists(filePath)) {
+            String contentType = Files.probeContentType(filePath);
+            if (!StringUtils.isEmpty(contentType)) {
+                response.setContentType(contentType);
+            }
+            request.setAttribute(NonStaticResourceHttpRequestHandler.ATTR_FILE, filePath);
+            nonStaticResourceHttpRequestHandler.handleRequest(request, response);
+        } else {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            response.setCharacterEncoding(StandardCharsets.UTF_8.toString());
+        }
     }
 
 
-
 }
-
